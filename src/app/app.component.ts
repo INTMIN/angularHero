e4fd55df596fc02e14d9e483a36dd35bea1981ab
MIN <1316914661@qq.com>
2019-04-16 17:10:50 +0800

    angular项目字段更新

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Hero test of angular';
+  title = 'angular的小demo';
 }
--- src/app/mock-heros.ts
+++ src/app/mock-heros.ts
@@ -1,14 +1,14 @@
 import { Hero } from './hero';
 
 export const HEROES: Hero[] = [
-  { id: 11, name: 'Mr. Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
+  { id: 11, name: '小明' },
+  { id: 12, name: '小红' },
+  { id: 13, name: '张三' },
+  { id: 14, name: '测试人员' },
+  { id: 15, name: '开发人员' },
+  { id: 16, name: '架构人员' },
+  { id: 17, name: 'react框架' },
+  { id: 18, name: 'vue框架' },
+  { id: 19, name: 'angular框架' },
+  { id: 20, name: 'ui设计师' }
 ];
